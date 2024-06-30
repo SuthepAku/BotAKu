24951a5e3f52ea95907f09a6301fc80e66102717
SuthepAku <suthep1234562002@gmail.com>
2024-06-30 21:45:35 +0700

    Ubot.py

--- bot.py
+++ bot.py
@@ -21,15 +21,15 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
-    channel = client.get_channel(1256359434133835817)
+    channel = client.get_channel(1256977666414874764)
     if channel is not None:
-        tex = f"{member.mention} Welcomeแนะนำตัวด้วยนะ คำสังคือ*My ตามนี้แป๊ะๆ จากนั้นทำตามที่บอทบอก {member.guild.name}!"
+        tex = f"{member.mention} Welcome แนะนำตัวด้วยนะ พิมพ์/เลือกแ นะนำตัว จากนั้นทำตามที่บอทบอก {member.guild.name}!"
         await channel.send(tex)
 
 
 @client.event
 async def on_member_remove(member):
-    channel = client.get_channel(1256614468587225139)
+    channel = client.get_channel(1256977691715047424)
     if channel is not None:
         tex = f"{member.mention} Bye from {member.guild.name}!"
         await channel.send(tex)
@@ -72,13 +72,13 @@ async def namecommand(interaction: discord.Interaction, name: str):
     await interaction.response.defer(thinking=True)
 
     # ส่งข้อความไปยังช่องอื่น
-    channel_id = 1256313588080181310  # แทนที่ด้วย ID ของช่องที่ต้องการส่งข้อความไป
+    channel_id = 1079367503525974078  # แทนที่ด้วย ID ของช่องที่ต้องการส่งข้อความไป
     channel = client.get_channel(channel_id)
     if channel is not None:
         await channel.send(f'ผู้ใช้ {interaction.user.mention} ได้แนะนำตัวว่า: {name}')
 
     # ตอบกลับการอินเตอร์แอคชั่น
-    await interaction.followup.send('ok!')
+    await interaction.followup.send('ok!แนะนำตัวเสร็จแล้ว โปรพิมพ์*K ตัวพิมพ์ใหญ่นะ เพื่อเข้ามาEnjoy')
 
 
 @client.event
@@ -86,16 +86,16 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content == '!giveRole':
-        role_name = "220"
+    if message.content == '*K':
+        role_name = "1101"
         role = discord.utils.get(message.guild.roles, name=role_name)
 
         if role:
             try:
                 await message.author.add_roles(role)
-                await message.channel.send(f"ได้รับบทบาท {role.name} แล้ว!")
+                await message.channel.send(f"ได้รับ {role.name} แล้ว!")
             except discord.Forbidden:
-                await message.channel.send("ไม่สามารถมอบบทบาทให้ผู้ใช้งานได้")
+                await message.channel.send("ไม่สามารถมอบให้ผู้ใช้งานได้")
         else:
             await message.channel.send("ไม่พบบทบาทที่ต้องการในเซิร์ฟเวอร์")
 
